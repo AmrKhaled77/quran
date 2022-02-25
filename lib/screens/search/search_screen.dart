1967c893e4476245faf8b7567317c763e16d26fb
AmrKhaled77 <amrk4912@gmail.com>
2022-02-25 19:22:55 +0200

    light mode

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -16,102 +16,111 @@ class SearchScreen extends StatelessWidget {
             body: Stack(
               children: [
                 Image(
-                  image: AssetImage('assets/images/backgroundupperdraw.png'),
+                  image: appCubit.get(context).isDark?
+                  AssetImage('assets/images/backgroundupperdraw.jpg'):
+                  AssetImage('assets/images/backgroundupperdraw.png'),
                   width: double.infinity,
                   height: double.infinity,
                   fit: BoxFit.fill,
                 ),
 
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Container(
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Container(
 
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextFormField(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
 
-                            controller: searchController,
-                            keyboardType: TextInputType.text,
-                            validator: (String value){
-                              if(value.isEmpty){
-                                return 'search must not be empty';
-                              }return null;
-                            },
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.blueAccent,
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide.none,
-                                  borderRadius: BorderRadius.circular(50)
+                              controller: searchController,
+                              keyboardType: TextInputType.text,
+                              validator: (String value){
+                                if(value.isEmpty){
+                                  return 'search must not be empty';
+                                }return null;
+                              },
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.black45,
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius: BorderRadius.circular(50)
+                                ),
+                                prefixIcon:Icon(Icons.search),
+                                labelText: 'search',
                               ),
-                              prefixIcon:Icon(Icons.search),
-                              labelText: 'search',
+                              onFieldSubmitted: (value){
+                                appCubit.get(context).getSearchVerses(value);
+                              },
+
+
                             ),
-                            onFieldSubmitted: (value){
-                              appCubit.get(context).getSearchVerses(value);
-                            },
-
-
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 12, right: 12, top: 25),
-                      child: Container(
-                        height: MediaQuery.of(context).size.height * 0.75,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Color.fromARGB(255, 16, 15, 54),
-                        ),
-                        child: Column(
-                          children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 12, right: 12, top: 25),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.75,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: appCubit.get(context).isDark?Colors.white:Color.fromARGB(255, 16, 15, 54),
+                          ),
+                          child: Column(
+                            children: [
 
-                            Expanded(
-                              child:   ListView.separated(itemBuilder: (context,index){
+                              Expanded(
+                                child:   ListView.separated(itemBuilder: (context,index){
 
-                                return  Container(
-                                  child: Text(appCubit.get(context).search[index]['text'],
-                                    style: TextStyle(color: Colors.white,fontWeight:FontWeight.w300,
-                                        fontSize: 20
-                                    ),
-                                  ),
-                                );
+                                  return  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      child: Text(appCubit.get(context).search[index]['text'],
+                                        style: TextStyle(color: appCubit.get(context).isDark?Colors.black:Colors.white,
 
-                              },
-
-                                  physics: BouncingScrollPhysics(),
-
-                                  separatorBuilder: (context,index){
-
-                                    return  Padding(
-
-                                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-
-                                      child: Container(
-
-                                        height: 1,
-
-                                        color: Color.fromARGB(255, 87, 89, 116),
-
+                                            fontWeight:FontWeight.w300,
+                                            fontSize: 20
+                                        ),
                                       ),
+                                    ),
+                                  );
 
-                                    );
+                                },
 
-                                  },
+                                    physics: BouncingScrollPhysics(),
 
-                                  itemCount: appCubit.get(context).search.length),
-                            )
+                                    separatorBuilder: (context,index){
 
-                          ],
+                                      return  Padding(
+
+                                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+
+                                        child: Container(
+
+                                          height: 1,
+
+                                          color: Color.fromARGB(255, 87, 89, 116),
+
+                                        ),
+
+                                      );
+
+                                    },
+
+                                    itemCount: appCubit.get(context).search.length),
+                              )
+
+                            ],
+                          ),
                         ),
                       ),
-                    ),
 
 
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
